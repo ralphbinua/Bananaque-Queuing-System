3ec83a0e542122b7b2b1f84ecab6a299da1f7059
Ralph Binua <binua.ralphgabrielb@gmail.com>
2026-05-09 02:02:47 +0800

    render update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:5000/api";
+
 
 const api = axios.create({
   baseURL: BASE_URL,
